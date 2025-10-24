7e4d757ee43109de96881c9988f8798672253398
ArayikK <159683807+ArayikK@users.noreply.github.com>
2025-10-24 08:45:06 +0400

    Add files via upload

---
+++ Strassen.py
@@ -0,0 +1,21 @@
+def add_matrix(A, B):
+    n = len(A)
+    return [[A[i][j] + B[i][j] for j in range(n)] for i in range(n)]
+
+def sub_matrix(A, B):
+    n = len(A)
+    return [[A[i][j] - B[i][j] for j in range(n)] for i in range(n)]
+
+def split_matrix(M):
+    n = len(M)
+    mid = n // 2
+    A11 = [row[:mid] for row in M[:mid]]
+    A12 = [row[mid:] for row in M[:mid]]
+    A21 = [row[:mid] for row in M[mid:]]
+    A22 = [row[mid:] for row in M[mid:]]
+    return A11, A12, A21, A22
+
+def combine_quadrants(C11, C12, C21, C22):
+    top = [C11[i] + C12[i] for i in range(len(C11))]
+    bottom = [C21[i] + C22[i] for i in range(len(C21))]
+    return top + bottom
